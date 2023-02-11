085ca930d51085814b92c1c0da0653a0b99d2a75
jingchengpeng@yahoo.com <jingchengpeng@yahoo.com>
2023-02-11 11:36:25 -0500

    project info page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
-import Clients from "./components/Clients";
-import Projects from "./components/Projects";
-
-import AddClientModel from "./components/AddClientModel";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import Project from "./pages/Project";
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
   cache: new InMemoryCache(),
@@ -12,12 +13,16 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className="container">
-          <AddClientModel />
-          <Projects />
-          <Clients />
-        </div>
+        <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects/:id" element={<Project />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
       </ApolloProvider>
     </>
   );
