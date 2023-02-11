2a2d6654248dd919f06dedcc0dd9677e9b4ae257
jingchengpeng@yahoo.com <jingchengpeng@yahoo.com>
2023-02-10 22:47:42 -0500

    started frontend

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1>Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
