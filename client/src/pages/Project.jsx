2efc67e90cc3bbcaa467eb4824f89f03b21746dd
jingchengpeng@yahoo.com <jingchengpeng@yahoo.com>
2023-02-12 16:42:02 -0500

    basic structure working

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,14 +1,17 @@
 import { Link, useParams } from "react-router-dom";
+import ClientInfo from "../components/ClientInfo";
+import DeleteProjectButton from "../components/DeleteProjectButton";
+import EditProjectForm from "../components/EditProjectForm";
 import { useQuery } from "@apollo/client";
 import { GET_PROJECT } from "../queries/projectQueries";
-import ClientInfo from "../components/ClientInfo";
 
 export default function Project() {
   const { id } = useParams();
   const { loading, error, data } = useQuery(GET_PROJECT, { variables: { id } });
 
-  if (loading) return <p>Loading</p>;
+  if (loading) return <p>Loading...</p>;
   if (error) return <p>Something Went Wrong</p>;
+
   return (
     <>
       {!loading && !error && (
@@ -22,7 +25,12 @@ export default function Project() {
 
           <h5 className="mt-3">Project Status</h5>
           <p className="lead">{data.project.status}</p>
+
           <ClientInfo client={data.project.client} />
+
+          <EditProjectForm project={data.project} />
+
+          <DeleteProjectButton projectId={data.project.id} />
         </div>
       )}
     </>
